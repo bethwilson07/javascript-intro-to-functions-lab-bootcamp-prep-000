70d74a9ce5c6e595109981dcb8cdd00d7adb2c8d
Beth Wilson <beth.ny.wlsn@gmail.com>
2018-07-18 20:30:47 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(string) {
+  return string.toUpperCase(
+)
+}
+
+function whisper(string) {
+  return string.toLowerCase(
+)
+}
+
+function logShout(hello) {
+  console.log(('hello').toUpperCase())
+}
+
+function logWhisper(HELLO) {
+  console.log(('HELLO').toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  return string.logWhisper(
+)
+}
