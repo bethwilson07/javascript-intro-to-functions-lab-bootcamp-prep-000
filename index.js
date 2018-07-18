a7474885351434186e84e416b18c47586884cea0
Beth Wilson <beth.ny.wlsn@gmail.com>
2018-07-18 21:33:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,4 +23,7 @@ function sayHiToGrandma(string) {
   if (whisper(string) === string) {
   return "I can\`t hear you!"
   }
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
 }
